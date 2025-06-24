dc2d27704e6db42489877c5dcbc77e87dd7d8c9d
Ajitha Estharla <aestharl@amazon.com>
2025-06-24 14:31:38 -0700

    feat: add packages

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,205 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: 'main',
-  name: 'test-repo-npm',
-  projenrcTs: true,
+import { awscdk, JsonFile, Project, typescript } from "projen";
+import { TypeScriptAppProject } from "projen/lib/typescript";
 
+const projectMetadata = {
+  author: "Ajitha",
+  authorAddress: "",
+  repositoryUrl: "https://github.com/ajestharl/test-repo-npm.git",
+  cdkVersion: "2.189.1",
+  constructsVersion: "10.4.2",
+  defaultReleaseBranch: "main",
+  name: "ajithaproject",
+};
+export const configureMarkDownLinting = (tsProject: TypeScriptAppProject) => {
+  tsProject.addDevDeps(
+    "eslint-plugin-md",
+    "markdown-eslint-parser",
+    "eslint-plugin-prettier",
+  );
+  tsProject.eslint?.addExtends(
+    "plugin:md/recommended",
+    "plugin:prettier/recommended",
+  );
+  tsProject.eslint?.addOverride({
+    files: ["*.md"],
+    parser: "markdown-eslint-parser",
+    rules: {
+      "prettier/prettier": ["error", { parser: "markdown" }],
+      "@typescript-eslint/no-floating-promises": "off",
+      "@typescript-eslint/return-await": "off",
+      quotes: "off",
+    },
+  });
+  tsProject.eslint?.addRules({
+    "prettier/prettier": "error",
+    "md/remark": [
+      "error",
+      {
+        plugins: [
+          "preset-lint-markdown-style-guide",
+          ["lint-list-item-indent", "space"],
+        ],
+      },
+    ],
+  });
+};
+
+export const addTestTargets = (subProject: Project) => {
+  const eslintTask = subProject.tasks.tryFind("eslint");
+  const testTask = subProject.tasks.tryFind("test");
+  if (testTask && eslintTask) {
+    testTask.reset();
+    testTask.exec(
+      "jest --passWithNoTests --updateSnapshot --testPathIgnorePatterns=.*\\.accept\\.test\\.ts$",
+      {
+        receiveArgs: true,
+      },
+    );
+    testTask.spawn(eslintTask);
+  }
+
+  const acceptTask = subProject.addTask("accept", {
+    description: "Run all acceptance tests",
+  });
+  const defaultTask = subProject.tasks.tryFind("default");
+  if (defaultTask) acceptTask.spawn(defaultTask);
+
+  const preCompileTask = subProject.tasks.tryFind("pre-compile");
+  if (preCompileTask) acceptTask.spawn(preCompileTask);
+
+  const compileTask = subProject.tasks.tryFind("compile");
+  if (compileTask) acceptTask.spawn(compileTask);
+
+  const postCompileTask = subProject.tasks.tryFind("post-compile");
+  if (postCompileTask) acceptTask.spawn(postCompileTask);
+
+  acceptTask.exec("jest --passWithNoTests --updateSnapshot --group=accept", {
+    receiveArgs: true,
+  });
+};
+const project = new awscdk.AwsCdkConstructLibrary({
+  ...projectMetadata,
+  jsiiVersion: "~5.7.0",
+  projenrcTs: true,
+  docgen: true,
+  github: true,
+  gitignore: [".idea", "API.md"],
+  eslint: true,
+  eslintOptions: {
+    prettier: true,
+    fileExtensions: [".ts", ".md"],
+    dirs: ["src", "test", "docs"],
+  },
+  jestOptions: {
+    jestConfig: {
+      verbose: true,
+    },
+  },
+  cdkVersionPinning: false,
+  release: false,
+  autoMerge: false,
+  releaseToNpm: false,
+  constructsVersion: "10.4.2",
+  packageName: "@example/test-repo-npm",
+  description: "Test Package",
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+if (project.github) {
+  const buildWorkflow = project.github?.tryFindWorkflow("build");
+  if (buildWorkflow && buildWorkflow.file) {
+    buildWorkflow.file.addOverride("jobs.build.permissions.contents", "read");
+    buildWorkflow.file.addOverride("jobs.build.env", {
+      CI: "true",
+      // Increasing heap size to mitigate potential "heap out of memory" errors during ESLint execution.
+      // TODO: Need to find a better way to do this, but this works for now.
+      NODE_OPTIONS: "--max-old-space-size=8192",
+    });
+  }
+}
+// Add Lerna configuration file (lerna.json)
+new JsonFile(project, "lerna.json", {
+  obj: {
+    packages: ["src/packages/*", "src/packages/smithy/build/smithy/source/*"],
+    version: "0.0.0",
+    npmClient: "yarn",
+  },
+});
+project.package.file.addOverride("private", true);
+project.package.file.addOverride("workspaces", [
+  "src/packages/*",
+  "src/packages/smithy/build/smithy/source/*",
+]);
+// Run Lerna build one package at a time and,
+// waits for each package to complete before showing its logs.
+project.preCompileTask.exec("npx lerna run build --concurrency=1 --no-stream");
+project.addScripts({
+  "import-private-key":
+    "ts-node src/packages/app-framework-ops-tools/src/importPrivateKey.ts",
+  "get-table-name":
+    "ts-node src/packages/app-framework-ops-tools/src/getTableName.ts",
+});
+
+addTestTargets(project);
+configureMarkDownLinting(project);
+
+interface PackageConfig {
+  name: string;
+  outdir: string;
+  deps?: string[];
+  devDeps?: string[];
+  bundledDeps?: string[];
+}
+const addPrettierConfig = (projectType: Project) => {
+  new JsonFile(projectType, ".prettierrc.json", {
+    obj: {
+      singleQuote: true,
+      trailingComma: "all",
+    },
+  });
+};
+
+export const createPackage = (config: PackageConfig) => {
+  const tsProject = new awscdk.AwsCdkConstructLibrary({
+    ...projectMetadata,
+    name: config.name,
+    outdir: config.outdir,
+    parent: project,
+    deps: config.deps,
+    devDeps: config.devDeps,
+    bundledDeps: config.bundledDeps,
+    docgen: false,
+    packageName: config.name,
+    release: false,
+    releaseToNpm: false,
+  });
+  addTestTargets(tsProject);
+  addPrettierConfig(tsProject);
+  configureMarkDownLinting(tsProject);
+  tsProject.package.file.addOverride("private", false);
+  return tsProject;
+};
+
+createPackage({
+  name: "ajithapack1",
+  outdir: "src/packages/ajithapack1",
+});
+
+const package2 = new typescript.TypeScriptProject({
+  ...projectMetadata,
+  name: "ajithapack2",
+  outdir: "src/packages/ajithapack2",
+  parent: project,
+  projenrcTs: false,
+  release: false,
+  releaseToNpm: false,
+  repository: projectMetadata.repositoryUrl,
+});
+addTestTargets(package2);
+addPrettierConfig(package2);
+configureMarkDownLinting(package2);
+package2.package.file.addOverride("private", false);
+
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="@example/test-repo-npm.Hello"></a>
+
+#### Initializers <a name="Initializers" id="@example/test-repo-npm.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from '@example/test-repo-npm'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#@example/test-repo-npm.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="@example/test-repo-npm.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
